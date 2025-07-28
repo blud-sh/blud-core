8973a7d7fc8f33296d3cbde58cffb3913323900e
Ethics03 <honey032004@gmail.com>
2025-07-29 02:07:10 +0530

    package changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AblyModule } from './ably/ably.module';
 
 @Module({
-  imports: [ChatModule, AuthModule, PrismaModule],
+  imports: [ChatModule, AuthModule, PrismaModule, AblyModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
