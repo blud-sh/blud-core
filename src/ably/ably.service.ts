f052c00905eb3d97a0f2b563854b4457ea845920
Ethics03 <honey032004@gmail.com>
2025-07-29 02:11:34 +0530

    refactor: client status check

--- src/ably/ably.service.ts
+++ src/ably/ably.service.ts
@@ -24,12 +24,16 @@ export class AblyService implements OnModuleInit , OnModuleDestroy {
 
     this.chatClient = new ChatClient(this.ablyReal);
 
-    this.chatClient.connection.onStatusChange((status: ConnectionStatusChange) => {
-      this.logger.log(`Connection status: ${status.current}`);
+     const { off: unsubscribeConnectionStatus } =
+    this.chatClient.connection.onStatusChange((change: ConnectionStatusChange) => {
+      console.log("Connection state changed to", change.current);
     });
+
   }
   
+  
   async onModuleDestroy() {
-    
+    await this.ablyReal.close()
+    this.logger.log('Ably connection closed');
   }
 }
